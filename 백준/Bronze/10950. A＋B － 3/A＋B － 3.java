3e3d9804219bfe3f7b9b6b1459b9458b12494c16
ReaRendil <151796249+ReaRendil@users.noreply.github.com>
2024-07-31 15:50:34 +0900

    [Bronze V] Title: A+B - 3, Time: 228 ms, Memory: 17888 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int q;
+        int w;
+        for  (int a =0; a<t; a++){
+            q = sc.nextInt();
+            w = sc.nextInt();
+            System.out.println(q+w);
+        }
+    }
+}
