dc11b08e27a53bcec6d5f2a8fe0a0b552c23f125
ReaRendil <151796249+ReaRendil@users.noreply.github.com>
2024-09-10 17:29:55 +0900

    [Bronze III] Title: 꼬리를 무는 숫자 나열, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/1598. 꼬리를 무는 숫자 나열/꼬리를 무는 숫자 나열.c
+++ 백준/Bronze/1598. 꼬리를 무는 숫자 나열/꼬리를 무는 숫자 나열.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 int def (int a,int b){
     if(a>b){
@@ -11,20 +10,7 @@ int main()
 {
     int a,b;
     scanf("%d%d",&a,&b);
-    int A = a%4; int B = b%4;
-    if(A%4==0)A=4;
-    if(b%4==0)B=4;
-    int sum = def(A,B);
-    for(;;) {
-         if(a%4==0) break;
-         a++;
-}
-    for(;;){
-              if(b%4==0) break;
-        b++;
-    }
-    sum+= def(a/4,b/4);
-    
-    printf("%d",sum);
+    a--;b--;
+    printf("%d",def(a%4,b%4) + def(a/4,b/4));
     return 0;
 }
